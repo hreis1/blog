87b47c3a34fd9ef372eec93c3e66e0e24cf54a1b
reis <ph.reis@discente.ufma.br>
2024-04-22 15:35:17 -0300

    feat/adiciona buscar por tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,15 @@ class TagsController < ApplicationController
     @tag = Tag.find(params[:id])
     @posts = @tag.posts.active.order(created_at: :desc).page(params[:page]).per(3)
   end
+
+  def search
+    return redirect_to root_path, alert: t(".empty_search") if params[:tag].blank?
+
+    @tag = Tag.find_by(name: params[:tag])
+    if @tag
+      @posts = @tag.posts.active.order(created_at: :desc).page(params[:page]).per(3)
+      render :show
+    end
+    redirect_to root_path, alert: t(".not_found", tag: params[:tag])
+  end
 end
